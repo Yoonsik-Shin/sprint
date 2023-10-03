64d4c3fb9768d1a334721439865c4c529e349f14
Yoonsik-Shin <sys25800994@gmail.com>
2023-10-03 15:55:05 +0900

    ✨ feat: cORS 설정 추가

--- src/studies/entities/recruit.entity.ts
+++ src/studies/entities/recruit.entity.ts
@@ -1,5 +1,6 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, OneToOne, PrimaryGeneratedColumn } from 'typeorm';
 import { SuperEntity } from '../../commons/entities/super.entity';
+import { Study } from './study.entity';
 
 @Entity()
 export class Recruit extends SuperEntity<Recruit> {
